75afac5b5265c71085663759d9a9b2581c1f0522
codestruct <hello@helenshiu.com>
2017-06-01 01:48:49 -0400

    Achieve pagespeed above 90.

--- views/dist/js/main.js
+++ views/dist/js/main.js
@@ -1,4 +1,4 @@
-/*! PizzaWebPageOptimization - v - 2017-05-31 */// As you may have realized, this website randomly generates pizzas.
+/*! PizzaWebPageOptimization - v - 2017-06-01 */// As you may have realized, this website randomly generates pizzas.
 // Here are arrays of all possible pizza ingredients.
 var pizzaIngredients = {};
 pizzaIngredients.meats = [
@@ -361,7 +361,10 @@ var pizzaElementGenerator = function(i) {
   pizzaImageContainer.style.width="35%";
 
   pizzaImage.src = "dist/images_src/pizza.png";
-  pizzaImage.classList.add("img-responsive");
+  /* Add width and height attributes to img - HS */
+  pizzaImage.style.width = "100%";
+  pizzaImage.style.height = "100%";
+  //pizzaImage.classList.add("img-responsive");
   pizzaImageContainer.appendChild(pizzaImage);
   pizzaContainer.appendChild(pizzaImageContainer);
 
@@ -403,33 +406,6 @@ var resizePizzas = function(size) {
 
   changeSliderLabel(size);
 
-/*
-   // Returns the size difference to change a pizza element from one size to another. Called by changePizzaSlices(size).
-  function determineDx (elem, size) {
-    var oldWidth = elem.offsetWidth;
-    var windowWidth = document.querySelector("#randomPizzas").offsetWidth;
-    var oldSize = oldWidth / windowWidth;
-
-    // Changes the slider value to a percent width
-    function sizeSwitcher (size) {
-      switch(size) {
-        case "1":
-          return 0.25;
-        case "2":
-          return 0.3333;
-        case "3":
-          return 0.5;
-        default:
-          console.log("bug in sizeSwitcher");
-      }
-    }
-
-    var newSize = sizeSwitcher(size);
-    var dx = (newSize - oldSize) * windowWidth;
-
-    return dx;
-  }
-*/
   // Iterates through pizza elements on the page and changes their widths
   function changePizzaSizes(size) {
     switch(size) {
